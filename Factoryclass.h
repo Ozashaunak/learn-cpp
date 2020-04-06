164791075d9788293d5015bd54a4737f72aa7dd8
Shaunak Oza <62095267+Ozashaunak@users.noreply.github.com>
2020-04-06 17:05:06 +0530

    Add files via upload
    factory pattern code to access class instances

---
+++ Factoryclass.h
@@ -0,0 +1,16 @@
+
+#ifndef FACTORY_CLASS
+#define FACTORY_CLASS
+#include<iostream>
+#include "AbstractCalc.h"
+enum op_type{TRIGNO,LOG,GEN};
+class Factory
+{
+	public:
+		AbstractCalc *getop(op_type type);
+		
+};
+
+#endif
+
+
