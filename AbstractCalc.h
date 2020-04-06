0a0b977804c5b6a29fc977ff2249f1d5fd7b30ca
Shaunak Oza <62095267+Ozashaunak@users.noreply.github.com>
2020-04-06 17:04:02 +0530

    Add files via upload

---
+++ AbstractCalc.h
@@ -0,0 +1,45 @@
+#ifndef CALC
+#define CALC
+class AbstractCalc
+{
+	public: 
+	virtual void getdata()=0;
+};
+class GeneralCal:public AbstractCalc
+{
+	public:
+		float a,b;
+		public:
+			void getdata();
+			void cAdd();
+			void cSub();
+			void cMult();
+			void cDiv();
+	
+};
+
+class Trignometric: public AbstractCalc
+{
+	protected:
+		float a;
+		public:
+			 void getdata();
+			 void Sin();
+			 void Cos();
+			 void Tan();
+			 void SinH();
+			 void CosH();
+			 void TanH();
+			 
+};
+class Logarithmic:public AbstractCalc
+{
+	protected:
+		float a;
+		public:
+			void getdata();
+			void Log();
+			void LogTen();
+	
+};
+#endif
